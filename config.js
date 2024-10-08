c32aa401c5e77356e07d9d56354ff690c8356db4
ZZ <145645832+Shadow1088@users.noreply.github.com>
2024-10-08 18:28:02 +0200

    idc, the token doesnt have any important privileges

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-  GITHUB_TOKEN: "", // Will be populated by GitHub Actions
-  GIST_ID: "", // Will be populated by GitHub Actions
+  GITHUB_TOKEN: "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", // Will be populated by GitHub Actions
+  GIST_ID: "2ec6105a7ff830fb7268427e57337d60", // Will be populated by GitHub Actions
 };
 
 export default config;
