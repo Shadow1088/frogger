998bc760b012a60982a796e6747f82acbc936d8a
Shadow1088 <Shadow1088@users.noreply.github.com>
2024-10-08 15:40:21 +0000

    deploy: 42ad197794ceb7b9380301cb541f1896cfb11080

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  GITHUB_TOKEN: '',
-  GIST_ID: '',
+  GITHUB_TOKEN: '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',
+  GIST_ID: '2ec6105a7ff830fb7268427e57337d60',
 };
 export default config;
