d6122f287cf1c678c093e97b5e3d6498605d4c6c
ZZ <it2313@sspu-opava.cz>
2024-10-08 16:28:49 +0200

    Does config.js clear out important constants?

--- config.js
+++ config.js
@@ -5,11 +5,15 @@ const config = {
 };
 
 // This will load the environment variables if the file exists
+//
+//
+/* UNCOMMENT THIS IF RUNNING ON LOCALHOST AND HAVE VALID ./.ENV.JS
 try {
   const env = await import("./.env.js");
   Object.assign(config, env.default);
 } catch (error) {
   console.warn("No env.js file found. Using empty configuration.");
 }
+*/
 
 export default config;
