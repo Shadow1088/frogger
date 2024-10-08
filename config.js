1f5b8ca9bcd99e538689e75445522a3596eb035c
ZZ <145645832+Shadow1088@users.noreply.github.com>
2024-10-08 18:30:52 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  GITHUB_TOKEN: "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", // Will be populated by GitHub Actions
+  GITHUB_TOKEN: "gh"+"p_tL"+"5jyi3VYpRDLX5X"+"FgZe5L2"+"UR5EjWb0B0sZG", // Will be populated by GitHub Actions
   GIST_ID: "2ec6105a7ff830fb7268427e57337d60", // Will be populated by GitHub Actions
 };
 
