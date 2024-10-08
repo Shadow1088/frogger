2373d8a4136dc0f8e9960d595ada5f9df0559fbc
ZZ <it2313@sspu-opava.cz>
2024-10-08 16:53:50 +0200

    Deplozed, but unsuccessful, hmm

--- config.js
+++ config.js
@@ -1,19 +1,6 @@
-// config.js
 const config = {
-  GITHUB_TOKEN: "", // Will be populated from .env.js
-  GIST_ID: "", // Will be populated from .env.js
+  GITHUB_TOKEN: "", // Will be populated by GitHub Actions
+  GIST_ID: "", // Will be populated by GitHub Actions
 };
-
-// This will load the environment variables if the file exists
-//
-//
-/* UNCOMMENT THIS IF RUNNING ON LOCALHOST AND HAVE VALID ./.ENV.JS
-try {
-  const env = await import("./.env.js");
-  Object.assign(config, env.default);
-} catch (error) {
-  console.warn("No env.js file found. Using empty configuration.");
-}
-*/
 
 export default config;
